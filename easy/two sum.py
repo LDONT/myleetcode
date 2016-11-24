d7fca3f4f15003b6a56e68f1929845296f62df10
LDONT <2293550755@qq.com>
2016-11-24 21:37:03 +0800

    1

---
+++ easy/two sum.py
@@ -0,0 +1,27 @@
+﻿#1. two sum    5392ms
+class Solution(object):  
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for index in range(len(nums)):
+            for index2 in range(index+1,len(nums)):
+                if nums[index]+nums[index2]==target:
+                    return [index,index2]
+                    break
+#1. two sum*    #65ms
+class Solution(object): 
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        process={}
+        for index in range(len(nums)):
+            if target-nums[index] in process:
+                return [process[target-nums[index]],index]
+                break
+            process[nums[index]]=index 
